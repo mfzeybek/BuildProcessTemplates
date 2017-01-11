d2914be4f9bbd2ee86a15d573602bf5c60eb1d27
mfzeybek <fatih@osmansenoz.com>
2017-01-11 23:40:46 +0300

    ilk

---
+++ DXApplication1/KasaSatis/frmKasaRaporu.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace KasaSatis
+{
+    partial class frmKasaRaporu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.btnZRaporuAl = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.txtKasaBakiyesi = new DevExpress.XtraEditors.TextEdit();
+            this.txtGiderToplami = new DevExpress.XtraEditors.TextEdit();
+            this.txtKasiyer = new DevExpress.XtraEditors.TextEdit();
+            this.txtNakit = new DevExpress.XtraEditors.TextEdit();
+            this.txtKredi = new DevExpress.XtraEditors.TextEdit();
+            this.txtToplam = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.btnKapat = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKasaBakiyesi.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGiderToplami.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKasiyer.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNakit.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKredi.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtToplam.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(30, 184);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(189, 33);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "Nakit Toplamı";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(28, 280);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(189, 33);
+            this.labelControl3.TabIndex = 1;
+            this.labelControl3.Text = "Kredi Toplamı";
+            // 
+            // btnZRaporuAl
+            // 
+            this.btnZRaporuAl.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.btnZRaporuAl.Appearance.Options.UseFont = true;
+            this.btnZRaporuAl.Location = new System.Drawing.Point(50, 705);
+            this.btnZRaporuAl.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnZRaporuAl.Name = "btnZRaporuAl";
+            this.btnZRaporuAl.Size = new System.Drawing.Size(230, 75);
+            this.btnZRaporuAl.TabIndex = 2;
+            this.btnZRaporuAl.Text = "Z Raporu Al";
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.labelControl5.Appearance.Options.UseFont = true;
+            this.labelControl5.Location = new System.Drawing.Point(28, 84);
+            this.labelControl5.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(186, 33);
+            this.labelControl5.TabIndex = 0;
+            this.labelControl5.Text = "Kasa Bakiyesi";
+            // 
+            // txtKasaBakiyesi
+            // 
+            this.txtKasaBakiyesi.Location = new System.Drawing.Point(242, 69);
+            this.txtKasaBakiyesi.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtKasaBakiyesi.Name = "txtKasaBakiyesi";
+            this.txtKasaBakiyesi.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKasaBakiyesi.Properties.Appearance.Options.UseFont = true;
+            this.txtKasaBakiyesi.Size = new System.Drawing.Size(362, 52);
+            this.txtKasaBakiyesi.TabIndex = 3;
+            // 
+            // txtGiderToplami
+            // 
+            this.txtGiderToplami.Location = new System.Drawing.Point(808, 159);
+            this.txtGiderToplami.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtGiderToplami.Name = "txtGiderToplami";
+            this.txtGiderToplami.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtGiderToplami.Properties.Appearance.Options.UseFont = true;
+            this.txtGiderToplami.Size = new System.Drawing.Size(362, 52);
+            this.txtGiderToplami.TabIndex = 4;
+            // 
+            // txtKasiyer
+            // 
+            this.txtKasiyer.Location = new System.Drawing.Point(808, 69);
+            this.txtKasiyer.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtKasiyer.Name = "txtKasiyer";
+            this.txtKasiyer.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKasiyer.Properties.Appearance.Options.UseFont = true;
+            this.txtKasiyer.Size = new System.Drawing.Size(362, 52);
+            this.txtKasiyer.TabIndex = 5;
+            // 
+            // txtNakit
+            // 
+            this.txtNakit.Location = new System.Drawing.Point(242, 169);
+            this.txtNakit.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtNakit.Name = "txtNakit";
+            this.txtNakit.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNakit.Properties.Appearance.Options.UseFont = true;
+            this.txtNakit.Size = new System.Drawing.Size(362, 52);
+            this.txtNakit.TabIndex = 7;
+            // 
+            // txtKredi
+            // 
+            this.txtKredi.Location = new System.Drawing.Point(242, 264);
+            this.txtKredi.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtKredi.Name = "txtKredi";
+            this.txtKredi.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKredi.Properties.Appearance.Options.UseFont = true;
+            this.txtKredi.Size = new System.Drawing.Size(362, 52);
+            this.txtKredi.TabIndex = 8;
+            // 
+            // txtToplam
+            // 
+            this.txtToplam.Location = new System.Drawing.Point(242, 338);
+            this.txtToplam.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtToplam.Name = "txtToplam";
+            this.txtToplam.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtToplam.Properties.Appearance.Options.UseFont = true;
+            this.txtToplam.Size = new System.Drawing.Size(362, 52);
+            this.txtToplam.TabIndex = 9;
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.labelControl6.Appearance.Options.UseFont = true;
+            this.labelControl6.Location = new System.Drawing.Point(682, 73);
+            this.labelControl6.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(101, 33);
+            this.labelControl6.TabIndex = 0;
+            this.labelControl6.Text = "Kasiyer";
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.btnKapat.Appearance.Options.UseFont = true;
+            this.btnKapat.Location = new System.Drawing.Point(920, 700);
+            this.btnKapat.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(230, 75);
+            this.btnKapat.TabIndex = 2;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.Location = new System.Drawing.Point(28, 353);
+            this.labelControl4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(186, 33);
+            this.labelControl4.TabIndex = 0;
+            this.labelControl4.Text = "Genel Toplam";
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.labelControl7.Appearance.Options.UseFont = true;
+            this.labelControl7.Location = new System.Drawing.Point(682, 162);
+            this.labelControl7.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(190, 33);
+            this.labelControl7.TabIndex = 0;
+            this.labelControl7.Text = "Gider Toplamı";
+            // 
+            // frmKasaRaporu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 808);
+            this.Controls.Add(this.txtToplam);
+            this.Controls.Add(this.txtKredi);
+            this.Controls.Add(this.txtNakit);
+            this.Controls.Add(this.txtKasiyer);
+            this.Controls.Add(this.txtGiderToplami);
+            this.Controls.Add(this.txtKasaBakiyesi);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.btnZRaporuAl);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.labelControl7);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl5);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "frmKasaRaporu";
+            this.Text = "frmKasaRaporu";
+            this.Load += new System.EventHandler(this.frmKasaRaporu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtKasaBakiyesi.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGiderToplami.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKasiyer.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNakit.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtKredi.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtToplam.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.SimpleButton btnZRaporuAl;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.TextEdit txtKasaBakiyesi;
+        private DevExpress.XtraEditors.TextEdit txtGiderToplami;
+        private DevExpress.XtraEditors.TextEdit txtKasiyer;
+        private DevExpress.XtraEditors.TextEdit txtNakit;
+        private DevExpress.XtraEditors.TextEdit txtKredi;
+        private DevExpress.XtraEditors.TextEdit txtToplam;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.SimpleButton btnKapat;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+    }
+}
---
+++ DXApplication1/KasaSatis/frmKasaRaporu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KasaSatis
+{
+    public partial class frmKasaRaporu : Form
+    {
+        public frmKasaRaporu()
+        {
+            InitializeComponent();
+        }
+
+        clsTablolar.Kasa.csKasaHareket KasaHareketi = new clsTablolar.Kasa.csKasaHareket();
+        clsTablolar.Kasa.csKasaHareket.KasaRapor Rapor = new clsTablolar.Kasa.csKasaHareket.KasaRapor();
+
+        SqlTransaction TrGenel;
+        int _KasaID = KasaSatis.Properties.Settings.Default.KasaID;
+        int _PosID = 3;//şimdilik tek pos var Onun ID si 3
+
+
+
+        private void frmKasaRaporu_Load(object sender, EventArgs e)
+        {
+            TrGenel = SqlConnections.GetBaglanti().BeginTransaction();
+
+            Rapor = KasaHareketi.KasaBakiyeVer(SqlConnections.GetBaglanti(), TrGenel, _KasaID);
+            //TrGenel.Commit();
+
+            txtKasaBakiyesi.EditValue = Rapor.KasaBakiye;
+            txtNakit.EditValue = Rapor.Alacak;
+            txtGiderToplami.EditValue = Rapor.Borc;
+
+            //TrGenel = SqlConnections.GetBaglanti().BeginTransaction();
+            Rapor = KasaHareketi.KasaBakiyeVer(SqlConnections.GetBaglanti(), TrGenel, 3); // bu sefer pos un bakiyelerini yüklüyoruz
+
+            // posun çıkışı olabiliyor mu bilmiyorum??
+            txtKredi.EditValue = Rapor.KasaBakiye;
+
+            TrGenel.Commit();
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
